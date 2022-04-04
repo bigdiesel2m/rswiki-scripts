a6ee0b37e9792103cf54f8a7f3a16a98e7e374ab
Dan Brause <bigdiesel2m@yahoo.com>
2022-04-04 19:58:01 -0400

    cleaner batching using a dedicated function

--- batcher/batcher.py
+++ batcher/batcher.py
@@ -5,21 +5,23 @@ import math
 with open('batcher/32627.json') as infile:
     data_json = json.load(infile)
 
-# print(data_json[1]['i'])
 batches = [] # empty list of batches, to be iterated upon
+
+def check_batches(item):
+    for j in range(len(batches)):
+        if item['i'] == batches[j]['i'] and item['j'] == batches[j]['j']:
+            return j
+    return None
 
 # loading objects into batches based on i,j grid
 for i in range(len(data_json)):
     if i%50==0: print('current step:',i)
-    batched = False
-    for j in range(len(batches)):
-        if data_json[i]['i'] == batches[j]['i'] and data_json[i]['j'] == batches[j]['j']:
-            # if the object's i,j match an existing batch, load it into that batch
-            batches[j]['list'].append(data_json[i])
-            batched = True
-    if batched == False:
+
+    batch = check_batches(data_json[i])
+    if batch != None:
+        batches[batch]['list'].append(data_json[i])
+    else:
         # else create a new batch and add the object as the first member
         batches.append({'i': data_json[i]['i'], 'j': data_json[i]['j'], 'list': [data_json[i]]})
-        print("new batch created")
 
 print(len(batches))
