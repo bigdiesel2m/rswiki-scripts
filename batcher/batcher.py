d757380ba004fb618d17964b92ea84433b141c1b
Dan Brause <bigdiesel2m@yahoo.com>
2022-04-05 15:57:18 -0400

    added better configuration and option support

--- batcher/batcher.py
+++ batcher/batcher.py
@@ -2,16 +2,23 @@ import sys
 import json
 import math
 
-with open('batcher/32627.json') as infile:
-    data_json = json.load(infile)
-
-# threshold for multi-chunk batches; set to 0 for separate chunks
-threshold = 1
+# configuration paramaters
+ids = ['29983', '29984', '29985']
+name = 'Lizardman corpse'
+threshold = 0 # threshold for multi-chunk batches; set to 0 for separate chunks
+outtype = 'locline' # set to 'locline' for locline output, otherwise outputs standard map
 
 # empty lists for later use
 batches = []
-superbatches = [] 
+superbatches = []
+data_json = []
 
+for i in range(len(ids)):
+    filename = 'batcher/' + ids[i] + '.json'
+    with open(filename) as infile:
+        data_json = data_json + json.load(infile)
+
+# define functions for batching, could maybe be combined into one function
 def check_batches(item):
     for j in range(len(batches)):
         if item['i'] == batches[j]['i'] and item['j'] == batches[j]['j']:
@@ -35,7 +42,7 @@ for i in range(len(data_json)):
         # else create a new batch and add the object as the first member
         batches.append({'i': data_json[i]['i'], 'j': data_json[i]['j'], 'list': [data_json[i]]})
 
-# optional, grouping batches based on configurable range
+# optional, grouping batches based on configurable threshold
 if threshold > 0:
     for i in range(len(batches)):
         superbatch = check_super(batches[i])
@@ -47,7 +54,6 @@ if threshold > 0:
             superbatches[superbatch]['j_max'] = max(superbatches[superbatch]['j_max'], batches[i]['j'])
         else:
             superbatches.append({'i': batches[i]['i'], 'i_min': batches[i]['i'], 'i_max': batches[i]['i'], 'j': batches[i]['j'], 'j_min': batches[i]['j'], 'j_max': batches[i]['j'], 'list': batches[i]['list']})
-    print(len(superbatches))
     batches = superbatches
 
 
@@ -60,7 +66,11 @@ for i in range(len(batches)):
     for j in range(len(planelist)):
         if len(planelist[j]) > 0:
             planelist[j] = '|'.join(planelist[j])
-            outlist.append('{{ObjectLocLine\n|name = Drapes\n|location = ' + str(batches[i]['i']) + ',' + str(batches[i]['j']) + ' - {{FloorNumber|uk=' + str(j) + '}}\n|members = Yes\n|mapID = -1\n|plane = ' + str(j) + '\n|' + planelist[j] + '\n|mtype = pin}}')
+            if outtype == 'locline':
+                outlist.append('{{ObjectLocLine\n|name = ' + name + '\n|location = ' + str(batches[i]['i']) + ',' + str(batches[i]['j']) + ' - {{FloorNumber|uk=' + str(j) + '}}\n|members = Yes\n|mapID = -1\n|plane = ' + str(j) + '\n|' + planelist[j] + '\n|mtype = pin}}')
+            else:
+                outlist.append('{{Map|name = ' + name + '|mapID = -1|plane = ' + str(j) + '|' + planelist[j] + '|mtype = pin}}')
+
 
 with open('batcher/output.txt', 'w') as outfile:
     outfile.write('\n'.join(outlist))
