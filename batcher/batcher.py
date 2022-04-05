35e34c21f90f728ae82b9a270be7698c10fbb3a3
Dan Brause <bigdiesel2m@yahoo.com>
2022-04-05 00:19:50 -0400

    added superbatching with adjustable threshold

--- batcher/batcher.py
+++ batcher/batcher.py
@@ -5,13 +5,25 @@ import math
 with open('batcher/32627.json') as infile:
     data_json = json.load(infile)
 
-batches = [] # empty list of batches, to be iterated upon
+# threshold for multi-chunk batches; set to 0 for separate chunks
+threshold = 1
+
+# empty lists for later use
+batches = []
+superbatches = [] 
 
 def check_batches(item):
     for j in range(len(batches)):
         if item['i'] == batches[j]['i'] and item['j'] == batches[j]['j']:
             return j
     return None
+
+def check_super(batch):
+    for j in range(len(superbatches)):
+        if (abs(batch['i'] - superbatches[j]['i_min']) <= threshold or abs(batch['i'] - superbatches[j]['i_max']) <= threshold) and (abs(batch['j'] - superbatches[j]['j_min']) <= threshold or abs(batch['j'] - superbatches[j]['j_max']) <= threshold):
+            return j
+    return None
+
 
 # loading objects into batches based on i,j grid
 for i in range(len(data_json)):
@@ -22,6 +34,22 @@ for i in range(len(data_json)):
     else:
         # else create a new batch and add the object as the first member
         batches.append({'i': data_json[i]['i'], 'j': data_json[i]['j'], 'list': [data_json[i]]})
+
+# optional, grouping batches based on configurable range
+if threshold > 0:
+    for i in range(len(batches)):
+        superbatch = check_super(batches[i])
+        if superbatch != None:
+            superbatches[superbatch]['list'] = superbatches[superbatch]['list'] + batches[i]['list']
+            superbatches[superbatch]['i_min'] = min(superbatches[superbatch]['i_min'], batches[i]['i'])
+            superbatches[superbatch]['i_max'] = max(superbatches[superbatch]['i_max'], batches[i]['i'])
+            superbatches[superbatch]['j_min'] = min(superbatches[superbatch]['j_min'], batches[i]['j'])
+            superbatches[superbatch]['j_max'] = max(superbatches[superbatch]['j_max'], batches[i]['j'])
+        else:
+            superbatches.append({'i': batches[i]['i'], 'i_min': batches[i]['i'], 'i_max': batches[i]['i'], 'j': batches[i]['j'], 'j_min': batches[i]['j'], 'j_max': batches[i]['j'], 'list': batches[i]['list']})
+    print(len(superbatches))
+    batches = superbatches
+
 
 # now that we've batched stuff up, it's time to turn those into locline templates
 outlist = []
