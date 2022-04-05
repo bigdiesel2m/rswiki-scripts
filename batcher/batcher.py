d06f7f3c9d0243e2094d8d4ce4a099315b742ffd
Dan Brause <bigdiesel2m@yahoo.com>
2022-04-04 21:02:55 -0400

    first pass on batch cleaning and output

--- batcher/batcher.py
+++ batcher/batcher.py
@@ -15,13 +15,27 @@ def check_batches(item):
 
 # loading objects into batches based on i,j grid
 for i in range(len(data_json)):
-    if i%50==0: print('current step:',i)
-
     batch = check_batches(data_json[i])
     if batch != None:
+        # if we have a batch with correct i,j add object to that batch
         batches[batch]['list'].append(data_json[i])
     else:
         # else create a new batch and add the object as the first member
         batches.append({'i': data_json[i]['i'], 'j': data_json[i]['j'], 'list': [data_json[i]]})
 
-print(len(batches))
+outlist = []
+for i in range(len(batches)):
+    list_0 = []
+    list_1 = []
+    list_2 = []
+    list_3 = []
+    for j in range(len(batches[i]['list'])):
+        print(batches[i]['list'][j])
+        if batches[i]['list'][j]['plane'] == 0:
+            list_0.append(str(64*batches[i]['list'][j]['i'] + batches[i]['list'][j]['x']) + ',' + str(64*batches[i]['list'][j]['j'] + batches[i]['list'][j]['y']))
+        elif batches[i]['list'][j]['plane'] == 1:
+            list_1.append(str(64*batches[i]['list'][j]['i'] + batches[i]['list'][j]['x']) + ',' + str(64*batches[i]['list'][j]['j'] + batches[i]['list'][j]['y']))
+        elif batches[i]['list'][j]['plane'] == 2:
+            list_2.append(str(64*batches[i]['list'][j]['i'] + batches[i]['list'][j]['x']) + ',' + str(64*batches[i]['list'][j]['j'] + batches[i]['list'][j]['y']))
+        elif batches[i]['list'][j]['plane'] == 3:
+            list_3.append(str(64*batches[i]['list'][j]['i'] + batches[i]['list'][j]['x']) + ',' + str(64*batches[i]['list'][j]['j'] + batches[i]['list'][j]['y']))
