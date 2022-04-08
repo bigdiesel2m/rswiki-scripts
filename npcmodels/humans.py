e9829e896e1dc92db91349ba9fba1c995602f1f0
Dan Brause <bigdiesel2m@yahoo.com>
2022-04-08 12:57:09 -0400

    more fiddling with humanoid filtering

--- npcmodels/humans.py
+++ npcmodels/humans.py
@@ -4,18 +4,44 @@ import math
 import os.path
 import glob
 
-index = 11461
-outlist = []
+# configuration parameters
+regenerate = False # set to true to regenerate NPC list before filtering
+badnames = ['Zombie', 'Skeleton', 'Mummy', 'Goblin', 'Dwarf', 'Combat stone', 'Ork', 'TzHaar-Xil', 'TzHaar-Ket', 'Aviansie', 'Zombie pirate',]
+badmodels = []
 
-for file in glob.glob("dump/npc_defs/*.json"):
-    with open(file) as infile:
-        data_json = json.load(infile)
-    if('id' in data_json and 'name' in data_json and 'models' in data_json):
-        if(len(data_json['models']) > 5):
-            outlist.append(str(data_json['id']) + '\t' + str(data_json['name']) + '\t' + str(len(data_json['models'])) + '\t' + str(min(data_json['models'])))
+def common_member(a, b):
+    if len(set(a).intersection(set(b))) > 0:
+        return(True)
+    return(False)
+
+# this section either regenerates the list of models based on some broad filters or uses the existing list
+outjson = []
+if regenerate:
+    for file in glob.glob("dump/npc_defs/*.json"):
+        with open(file) as infile:
+            data_json = json.load(infile)
+        if('id' in data_json and 'name' in data_json and 'models' in data_json):
+            if(len(data_json['models']) > 5):
+                outjson.append({'id': data_json['id'], 'name': data_json['name'], 'models': data_json['models']})
+        # else:
+        #     print(data_json['id'])
+    with open('npcmodels/output.json', 'w') as outfile:
+        json.dump(outjson, outfile)
+else:
+    with open('npcmodels/output.json') as infile:
+        outjson = json.load(infile)
+print(len(outjson))
+
+# this section adds some finer filters to pare down the list
+outtxt = []
+for i in range(len(outjson)):
+    if common_member(badnames, [outjson[i]['name']]):
+        continue
+    elif common_member(badmodels, outjson[i]['models']):
+        continue
     else:
-        print(file)
-        
+        outtxt.append(str(outjson[i]['id']) + '\t' + str(outjson[i]['name']) + '\t' + str(len(outjson[i]['models'])))
+print(len(outtxt))
 
 with open('npcmodels/output.txt', 'w') as outfile:
-    outfile.write('\n'.join(outlist))
+    outfile.write('\n'.join(outtxt))
