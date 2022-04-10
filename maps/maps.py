d94cf3b751fc9f7b9f23cb6bd2b4423df3ca9ba5
Dan Brause <bigdiesel2m@yahoo.com>
2022-04-10 10:55:16 -0400

    more map testing stuff

--- maps/maps.py
+++ maps/maps.py
@@ -1,4 +1,12 @@
-import imageio
+from PIL import Image
 
-im = imageio.imread('maps/map.png')
-print(im[3922][6777])
+im = Image.open('maps/map.png')
+
+print(im.format, im.size, im.mode)
+
+box = (2560, 2560, 2816, 2816)
+im = im.crop(box)
+
+print(im.format, im.size, im.mode)
+
+im.save('maps/crop.png')
