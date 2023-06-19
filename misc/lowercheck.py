e52745ec34fd00e5932238052aa8340ae8eb22a7
Daniel <bigdiesel2m@yahoo.com>
2023-06-19 00:44:36 -0400

    check for lowercase actions

--- misc/lowercheck.py
+++ misc/lowercheck.py
@@ -10,9 +10,13 @@ for file in glob.glob("dump/object_defs/*.json"):
 
     with open(file) as infile:
         data_json = json.load(infile)
-        if ('id' in data_json and 'name' in data_json):
-            if len(data_json['name']) > 0 and data_json['name'][0].islower() and not data_json['name'] == "null":
-                outputlist.append([data_json['id'], data_json['name']])
+        # if ('id' in data_json and 'name' in data_json):
+        #     if len(data_json['name']) > 0 and data_json['name'][0].islower() and not data_json['name'] == "null":
+        #         outputlist.append([data_json['id'], data_json['name']])
+        if ('id' in data_json and 'actions' in data_json):
+            for action in data_json['actions']:
+                if(action != None and action[0].islower()):
+                    outputlist.append([data_json['id'], data_json['name'], data_json['actions']])
 
 with open('misc/output.json', 'w') as outfile:
     json.dump(outputlist, outfile)
